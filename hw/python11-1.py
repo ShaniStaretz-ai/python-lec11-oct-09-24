ee631ed13d3ff70492bec06830fd79ea0a114142
Shani Staretz <shakimi88@gmail.com>
2024-10-10 10:57:51 +0300

    strings start

--- hw/python11-1.py
+++ hw/python11-1.py
@@ -19,13 +19,13 @@ else:
     print("not at least 1 True:", l_booleans)
 
 # f
-l_rand_two = [randint(-2, 2) for _ in range(5)]
+l_rand_two: list[int] = [randint(-2, 2) for _ in range(5)]
 print("random [-2:2] list:", l_rand_two)
 # g
-print("is all not zero:",all([x!=0 for x in l_rand_two]))
-#h
-print("at least 1 not zero:",any([x!=0 for x in l_rand_two]))
-#i
-print("all zero:",all([x==0 for x in l_rand_two]))
-#j
-print("at least 1 zero:",any([x==0 for x in l_rand_two]))
+print("is all not zero:", all([x != 0 for x in l_rand_two]))
+# h
+print("at least 1 not zero:", any([x != 0 for x in l_rand_two]))
+# i
+print("all zero:", all([x == 0 for x in l_rand_two]))
+# j
+print("at least 1 zero:", any([x == 0 for x in l_rand_two]))
---
+++ hw/python11-2.py
@@ -0,0 +1,25 @@
+me: str = "Shani Staretz Kfar Saba"
+# a
+print("size:", len(me))
+# b
+print("upper:", me.upper())
+# c
+print("lower:", me.lower())
+# d
+print("startswith:", me.startswith("Shani"))
+# e
+print("endswith:", me.endswith("Kfar Saba"))
+# f
+print("split:", me.split(" "))
+# g
+print("split:", me.replace(' ', '*').split("*"))
+# h
+print("center:", me.center(50, '='))
+# i
+print("4-end:", me[3:])  # including the 4 char
+# j
+print("start-4:", me[:4])  # including the 4 char
+# k
+print("even chars:", me[1::2])  # evens from second char
+# l
+print("title:", me.title())  # including the 4 char
