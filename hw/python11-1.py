f0ada7ca26859c9d14c6601c7409ed33b934dff6
Shani Staretz <shakimi88@gmail.com>
2024-10-10 10:20:02 +0300

    hw1-any, all functions

---
+++ hw/python11-1.py
@@ -0,0 +1,31 @@
+from random import choice, randint
+
+# a
+l_booleans: list[bool] = [choice([True, False]) for _ in range(3)]
+print("Boolean list:", l_booleans)
+# b
+print("is all True:", all(l_booleans))
+# c
+print("is at least 1 True:", any(l_booleans))
+# d
+if not any(l_booleans):
+    print("all False:", l_booleans)
+else:
+    print("not all False:", l_booleans)
+# e
+if not all(l_booleans):
+    print("at least 1 False:", l_booleans)
+else:
+    print("not at least 1 True:", l_booleans)
+
+# f
+l_rand_two = [randint(-2, 2) for _ in range(5)]
+print("random [-2:2] list:", l_rand_two)
+# g
+print("is all not zero:",all([x!=0 for x in l_rand_two]))
+#h
+print("at least 1 not zero:",any([x!=0 for x in l_rand_two]))
+#i
+print("all zero:",all([x==0 for x in l_rand_two]))
+#j
+print("at least 1 zero:",any([x==0 for x in l_rand_two]))
