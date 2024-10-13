bfb4901d6795b1c267404cdc696a5e869b6aa826
Shani Staretz <shakimi88@gmail.com>
2024-10-13 17:25:42 +0300

    fixed any/all hw-1

--- hw/python11-1.py
+++ hw/python11-1.py
@@ -16,10 +16,16 @@ print("is at least 1 False:", not all(l_booleans))
 l_rand_two: list[int] = [randint(-2, 2) for _ in range(5)]
 print("random [-2:2] list:", l_rand_two)
 # g
+# print("is all not zero:", all(l_rand_two))
 print("is all not zero:", all([x != 0 for x in l_rand_two]))
 # h
+# print("is all not zero:", any(l_rand_two))
 print("is at least 1 not zero:", any([x != 0 for x in l_rand_two]))
 # i
+print("is all zero:", not any(l_rand_two))
 print("is all zero:", all([x == 0 for x in l_rand_two]))
 # j
+print("is at least 1 zero:", not all(l_rand_two))
 print("is at least 1 zero:", any([x == 0 for x in l_rand_two]))
+# first option is better because without addition list
+# first option is correct only because the values can be zero
